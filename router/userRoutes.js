a07515a5ba1493c04e8c594b0b03d069a1c16c9d
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2023-09-07 17:46:24 +0530

    Update userRoutes.js

--- router/userRoutes.js
+++ router/userRoutes.js
@@ -5,11 +5,13 @@ const {
   userLogin,
   userUpdate,
   userDelete,
+  userCurrent,
 } = require("../controller/userControl");
 const { verifyToken } = require("../config/authVerify");
 
 router.post("/signup", userSignup);
 router.post("/login", userLogin);
+router.get("/current-user", verifyToken, userCurrent);
 router.put("/update/:userId", verifyToken, userUpdate);
 router.delete("/delete/:userId", verifyToken, userDelete);
 
